4a17564ac7e4cd31a30c8102aeb4e0f2f9e7acaf
fakhri <fakhrimuhammad1571@gmail.com>
2025-08-25 11:41:28 +0700

    Penambahan animasi scroll

--- Portofolio/script.js
+++ Portofolio/script.js
@@ -1,3 +1,4 @@
+// Menu toogle
 document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.getElementById("menu-toggle");
     const navbar = document.getElementById("navbar");
@@ -22,3 +23,22 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+// Scroll animation
+function revealOnScroll() {
+  const reveals = document.querySelectorAll('.reveal');
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 100; // seberapa jauh sebelum terlihat
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+window.addEventListener('scroll', revealOnScroll);
